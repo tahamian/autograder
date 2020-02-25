68a17cb7242ad3b8fd8e6ce47b398260249518a7
tahamian <tahamian12@gmail.com>
2020-02-24 23:40:49 -0500

    added unit tests for python code

--- marker/scripts/__init__.py
+++ marker/scripts/__init__.py
@@ -4,6 +4,7 @@ import os
 import sys
 import subprocess
 import click
+import ast
 
 __version__ = "0.1"
 
@@ -20,27 +21,53 @@ class Assignment:
         self.functions = functions
 
 
+class PythonLiteralOption(click.Option):
+
+    def type_cast_value(self, ctx, value):
+        try:
+            return ast.literal_eval(value)
+        except:
+            raise click.BadParameter(value)
+
+
 @click.command()
-@click.option('--filename', help='name of the file that being run')
+@click.option('--filename', help='name of the file that being run', type=click.Path())
 @click.option('--stdout', help='get a stdout of python file', default=False)
-@click.option('--functions', help='get the function output')
-# @click.pass_context
-def run_script(filename, stdout=False, functions=None, *args, **kwargs):
+@click.option('--functions', help='get the function output', cls=PythonLiteralOption, default=[])
+def run_script(filename, stdout, functions, *args, **kwargs):
     return get_result(filename, stdout, functions)
 
 
 def get_result(filename, stdout, functions):
     output = {
-        'stdout': None,
-        'functions': None
+        'stdout': {},
+        'functions': {}
     }
 
     if stdout:
         try:
-            output['stdout'] = subprocess.check_output(['python3', filename])
+            output['stdout'] = {'success': subprocess.check_output(['python3', filename])}
 
         except Exception as e:
-            raise Exception(str(os.getcwd()))
+            output['stdout'] = {'error': str(e)}
+
+    if bool(functions):
+
+        for func in functions:
+            name = func['name']
+            args = ''
+            if func['args']:
+                args = ','.join(func['args'])
+            try:
+                import_path = (os.path.splitext(filename)[0]).replace('/', '.')
+
+                command = 'from {} import *; print({}({}))'.format(import_path, name, args)
+                func_output = subprocess.check_output(['python3', '-c', command])
+
+                output['functions'][func['name']] = dict(success=func_output.decode('utf-8'))
+
+            except Exception as e:
+                output['functions'][func['name']] = {'error': str(e)}
 
     return output
 
---
+++ marker/scripts/tests/pythagorean_theorem.py
@@ -0,0 +1,5 @@
+import math
+
+
+def pythagorean(a, b):
+    return math.sqrt(a ** 2 + b ** 2)
